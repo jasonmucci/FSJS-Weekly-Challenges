c2827c04d0361a4c7e912fbd0a6ac6db8ca57a4e
jasonmucci <muccijason@gmail.com>
2016-09-08 20:00:40 -0400

    fixed

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,7 +1,29 @@
 //create function
   //control and return statements
 
+function processInput(message)
+{
+
+  if (message == "Hello")
+  {
+    return("Hello World");
+}
+    else {
+      return("You didn't say hello : (");
+    }
+}
+
 //Prompt user for input and store variable
+
+var userInput;
+
+userInput = prompt();
+
+var response = processInput(userInput);
+
+console.log(response);
+
+processInput(userInput);
 
 //Call function
 
